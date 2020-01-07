f9ae5f2ebdbe4e39049576172d02b891aef35f27
ilhomjon7 <Alizoda2017@gmail.com>
2020-01-07 15:02:14 -0500

    creat new class; ClassGit

--- TestNG/src/demo/VerifyTitle.java
+++ TestNG/src/demo/VerifyTitle.java
@@ -1,5 +1,6 @@
 package demo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -14,24 +15,32 @@ public class VerifyTitle {
 	@BeforeTest
 	public void openBrowser() {
 		driver = new ChromeDriver();
-		driver.get("https://facebook.com");
-		
+		//driver.get("https://facebook.com");
+		driver.get("https://admin97672.wixsite.com/mysite");
 		
 	}
 	
 	@Test	
 	public void titleVerification() {
-			String expectedTitle = "Facebook - Log In or Sign Up";
-			String actualTitle= driver.getTitle();
-			Assert.assertEquals(actualTitle, expectedTitle);
+		//driver.findElement(By.name("search")).sendKeys("aad");
+			String x = driver.findElement(By.xpath("//span[text()='WHY RUSLAN CARS']")).getText();
+		//driver.findElement(By.cssSelector("#SearchBox1846-inputwithoptions > div > div > button.buttonnext4131183088--root.Focusable2375082276--root.SearchBox61359390--button > span")).click();	
+		String b = "Why no m";
+		if (x==b) {
+			System.out.println("passed");
+			}
+			else {
 			
+			System.out.println("faild");
+			}
 	}
-	
-	@AfterTest
-	public void closeBrowser(){
-		driver.close();
-		
-	}
+	//span[@class = 'buttonnext4131183088--content']
+//	@AfterTest
+//	public void closeBrowser() throws InterruptedException{
+//		Thread.sleep(6000);
+//		driver.close();
+//		
+//	}
 	
 	
 
